495bd783830fc6c607ab42aeb8852a868992a44e
pixeltris <6952411+pixeltris@users.noreply.github.com>
2021-05-23 23:23:32 +0100

    Update data files to V6.0.0.47

---
+++ Build/Data/device/656801959/data/keymap.js
@@ -0,0 +1,1118 @@
+[
+	{
+		"KeyName": "Escape",
+		"Show": "Esc",
+		"LogicCode": 47,
+		"LocationCode": 0,
+		"Position": {
+			"Left": 168,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "1",
+		"Show": "1",
+		"LogicCode": 36,
+		"LocationCode": 2,
+		"Position": {
+			"Left": 222,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "2",
+		"Show": "2",
+		"LogicCode": 37,
+		"LocationCode": 3,
+		"Position": {
+			"Left": 276,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "3",
+		"Show": "3",
+		"LogicCode": 38,
+		"LocationCode": 4,
+		"Position": {
+			"Left": 330,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "4",
+		"Show": "4",
+		"LogicCode": 39,
+		"LocationCode": 5,
+		"Position": {
+			"Left": 384,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "5",
+		"Show": "5",
+		"LogicCode": 40,
+		"LocationCode": 6,
+		"Position": {
+			"Left": 439,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "6",
+		"Show": "6",
+		"LogicCode": 41,
+		"LocationCode": 7,
+		"Position": {
+			"Left": 492,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "7",
+		"Show": "7",
+		"LogicCode": 42,
+		"LocationCode": 8,
+		"Position": {
+			"Left": 547,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "8",
+		"Show": "8",
+		"LogicCode": 43,
+		"LocationCode": 9,
+		"Position": {
+			"Left": 601,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "9",
+		"Show": "9",
+		"LogicCode": 44,
+		"LocationCode": 10,
+		"Position": {
+			"Left": 655,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "0",
+		"Show": "0",
+		"LogicCode": 45,
+		"LocationCode": 11,
+		"Position": {
+			"Left": 708,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "-",
+		"Show": "-",
+		"LogicCode": 51,
+		"LocationCode": 12,
+		"Position": {
+			"Left": 764,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "=",
+		"Show": "=",
+		"LogicCode": 52,
+		"LocationCode": 13,
+		"Position": {
+			"Left": 817,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Backspace",
+		"Show": "←",
+		"LogicCode": 48,
+		"LocationCode": 14,
+		"Position": {
+			"Left": 871,
+			"Top": 166,
+			"Width": 108,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "insert",
+		"Show": "insert",
+		"LogicCode": 78,
+		"LocationCode": 16,
+		"Position": {
+			"Left": 979,
+			"Top": 166,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "LED-LEFT-1",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 20,
+		"Position": {
+			"Left": 156,
+			"Top": 166,
+			"Width": 10,
+			"Height": 10
+		}
+	},
+	{
+		"KeyName": "LED-RIGHT-1",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 21,
+		"Position": {
+			"Left": 1034,
+			"Top": 166,
+			"Width": 10,
+			"Height": 10
+		}
+	},
+	{
+		"KeyName": "Tab",
+		"Show": "Tab",
+		"LogicCode": 49,
+		"LocationCode": 22,
+		"Position": {
+			"Left": 168,
+			"Top": 220,
+			"Width": 82,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Q",
+		"Show": "Q",
+		"LogicCode": 26,
+		"LocationCode": 24,
+		"Position": {
+			"Left": 249,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "W",
+		"Show": "W",
+		"LogicCode": 32,
+		"LocationCode": 25,
+		"Position": {
+			"Left": 304,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "E",
+		"Show": "E",
+		"LogicCode": 14,
+		"LocationCode": 26,
+		"Position": {
+			"Left": 357,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "R",
+		"Show": "R",
+		"LogicCode": 27,
+		"LocationCode": 27,
+		"Position": {
+			"Left": 411,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "T",
+		"Show": "T",
+		"LogicCode": 29,
+		"LocationCode": 28,
+		"Position": {
+			"Left": 466,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Y",
+		"Show": "Y",
+		"LogicCode": 34,
+		"LocationCode": 29,
+		"Position": {
+			"Left": 520,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "U",
+		"Show": "U",
+		"LogicCode": 30,
+		"LocationCode": 30,
+		"Position": {
+			"Left": 574,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "I",
+		"Show": "I",
+		"LogicCode": 18,
+		"LocationCode": 31,
+		"Position": {
+			"Left": 628,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "O",
+		"Show": "O",
+		"LogicCode": 24,
+		"LocationCode": 32,
+		"Position": {
+			"Left": 682,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "P",
+		"Show": "P",
+		"LogicCode": 25,
+		"LocationCode": 33,
+		"Position": {
+			"Left": 737,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "[",
+		"Show": "[",
+		"LogicCode": 53,
+		"LocationCode": 34,
+		"Position": {
+			"Left": 790,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "]",
+		"Show": "]",
+		"LogicCode": 54,
+		"LocationCode": 35,
+		"Position": {
+			"Left": 844,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "|",
+		"Show": "|",
+		"LogicCode": 55,
+		"LocationCode": 37,
+		"Position": {
+			"Left": 898,
+			"Top": 220,
+			"Width": 82,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Delete",
+		"Show": "Del",
+		"LogicCode": 81,
+		"LocationCode": 38,
+		"Position": {
+			"Left": 979,
+			"Top": 220,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "LED-LEFT-2",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 42,
+		"Position": {
+			"Left": 156,
+			"Top": 291,
+			"Width": 10,
+			"Height": 10
+		}
+	},
+	{
+		"KeyName": "LED-RIGHT-2",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 43,
+		"Position": {
+			"Left": 1034,
+			"Top": 291,
+			"Width": 10,
+			"Height": 10
+		}
+	},
+	{
+		"KeyName": "Caps Lock",
+		"Show": "Caps L",
+		"LogicCode": 62,
+		"LocationCode": 44,
+		"Position": {
+			"Left": 168,
+			"Top": 273,
+			"Width": 95,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "A",
+		"Show": "A",
+		"LogicCode": 10,
+		"LocationCode": 46,
+		"Position": {
+			"Left": 263,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "S",
+		"Show": "S",
+		"LogicCode": 28,
+		"LocationCode": 47,
+		"Position": {
+			"Left": 317,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "D",
+		"Show": "D",
+		"LogicCode": 13,
+		"LocationCode": 48,
+		"Position": {
+			"Left": 371,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "F",
+		"Show": "F",
+		"LogicCode": 15,
+		"LocationCode": 49,
+		"Position": {
+			"Left": 425,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "G",
+		"Show": "G",
+		"LogicCode": 16,
+		"LocationCode": 50,
+		"Position": {
+			"Left": 479,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "H",
+		"Show": "H",
+		"LogicCode": 17,
+		"LocationCode": 51,
+		"Position": {
+			"Left": 533,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "J",
+		"Show": "J",
+		"LogicCode": 19,
+		"LocationCode": 52,
+		"Position": {
+			"Left": 587,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "K",
+		"Show": "K",
+		"LogicCode": 20,
+		"LocationCode": 53,
+		"Position": {
+			"Left": 641,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "L",
+		"Show": "L",
+		"LogicCode": 21,
+		"LocationCode": 54,
+		"Position": {
+			"Left": 695,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": ";",
+		"Show": ";",
+		"LogicCode": 56,
+		"LocationCode": 55,
+		"Position": {
+			"Left": 749,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "'",
+		"Show": "'",
+		"LogicCode": 57,
+		"LocationCode": 56,
+		"Position": {
+			"Left": 803,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Return",
+		"Show": "Enter",
+		"LogicCode": 46,
+		"LocationCode": 58,
+		"Position": {
+			"Left": 857,
+			"Top": 273,
+			"Width": 122,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Page Up",
+		"Show": "P U",
+		"LogicCode": 80,
+		"LocationCode": 60,
+		"Position": {
+			"Left": 979,
+			"Top": 273,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "LED-LEFT-3",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 64,
+		"Position": {
+			"Left": 156,
+			"Top": 423,
+			"Width": 10,
+			"Height": 10
+		}
+	},
+	{
+		"KeyName": "LED-RIGHT-3",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 65,
+		"Position": {
+			"Left": 1034,
+			"Top": 423,
+			"Width": 10,
+			"Height": 10
+		}
+	},
+	{
+		"KeyName": "Left Shift",
+		"Show": "Shift",
+		"LogicCode": 3,
+		"LocationCode": 66,
+		"Position": {
+			"Left": 168,
+			"Top": 328,
+			"Width": 124,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Z",
+		"Show": "Z",
+		"LogicCode": 35,
+		"LocationCode": 68,
+		"Position": {
+			"Left": 290,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "X",
+		"Show": "X",
+		"LogicCode": 33,
+		"LocationCode": 69,
+		"Position": {
+			"Left": 344,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "C",
+		"Show": "C",
+		"LogicCode": 12,
+		"LocationCode": 70,
+		"Position": {
+			"Left": 398,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "V",
+		"Show": "V",
+		"LogicCode": 31,
+		"LocationCode": 71,
+		"Position": {
+			"Left": 452,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "B",
+		"Show": "B",
+		"LogicCode": 11,
+		"LocationCode": 72,
+		"Position": {
+			"Left": 506,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "N",
+		"Show": "N",
+		"LogicCode": 23,
+		"LocationCode": 73,
+		"Position": {
+			"Left": 560,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "M",
+		"Show": "M",
+		"LogicCode": 22,
+		"LocationCode": 74,
+		"Position": {
+			"Left": 614,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": ",",
+		"Show": ",",
+		"LogicCode": 59,
+		"LocationCode": 75,
+		"Position": {
+			"Left": 668,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": ".",
+		"Show": ".",
+		"LogicCode": 60,
+		"LocationCode": 76,
+		"Position": {
+			"Left": 722,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "/",
+		"Show": "/",
+		"LogicCode": 61,
+		"LocationCode": 77,
+		"Position": {
+			"Left": 776,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Right Shift",
+		"Show": "Shift",
+		"LogicCode": 7,
+		"LocationCode": 79,
+		"Position": {
+			"Left": 830,
+			"Top": 328,
+			"Width": 95,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Up Arrow",
+		"Show": "↑",
+		"LogicCode": 87,
+		"LocationCode": 81,
+		"Position": {
+			"Left": 925,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Page Down",
+		"Show": "P D",
+		"LogicCode": 83,
+		"LocationCode": 82,
+		"Position": {
+			"Left": 979,
+			"Top": 328,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Left Control",
+		"Show": "Ctrl",
+		"LogicCode": 2,
+		"LocationCode": 88,
+		"Position": {
+			"Left": 168,
+			"Top": 383,
+			"Width": 68,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Left GUI",
+		"Show": "Win",
+		"LogicCode": 5,
+		"LocationCode": 89,
+		"Position": {
+			"Left": 236,
+			"Top": 383,
+			"Width": 68,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Left Alt",
+		"Show": "Alt",
+		"LogicCode": 4,
+		"LocationCode": 90,
+		"Position": {
+			"Left": 304,
+			"Top": 383,
+			"Width": 68,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "fn1",
+		"Show": "fnx1",
+		"LogicCode": 50,
+		"LocationCode": 92,
+		"Position": {
+			"Left": 370,
+			"Top": 383,
+			"Width": 131,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Space",
+		"Show": "Space",
+		"LogicCode": 109,
+		"LocationCode": 94,
+		"Position": {
+			"Left": 370,
+			"Top": 447,
+			"Width": 335,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "fn2",
+		"Show": "fnx2",
+		"LogicCode": 110,
+		"LocationCode": 95,
+		"Position": {
+			"Left": 505,
+			"Top": 383,
+			"Width": 131,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "fn3",
+		"Show": "fnx3",
+		"LogicCode": 111,
+		"LocationCode": 97,
+		"Position": {
+			"Left": 639,
+			"Top": 383,
+			"Width": 68,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Right Alt",
+		"Show": "Alt",
+		"LogicCode": 8,
+		"LocationCode": 98,
+		"Position": {
+			"Left": 709,
+			"Top": 383,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Fn",
+		"Show": "Fn",
+		"LogicCode": 0,
+		"LocationCode": 99,
+		"Position": {
+			"Left": 763,
+			"Top": 383,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "App",
+		"Show": "App",
+		"LogicCode": 1,
+		"LocationCode": 100,
+		"Position": {
+			"Left": 817,
+			"Top": 383,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Left Arrow",
+		"Show": "←",
+		"LogicCode": 85,
+		"LocationCode": 102,
+		"Position": {
+			"Left": 871,
+			"Top": 383,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Down Arrow",
+		"Show": "↓",
+		"LogicCode": 86,
+		"LocationCode": 103,
+		"Position": {
+			"Left": 925,
+			"Top": 383,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "Right Arrow",
+		"Show": "→",
+		"LogicCode": 84,
+		"LocationCode": 104,
+		"Position": {
+			"Left": 979,
+			"Top": 383,
+			"Width": 52,
+			"Height": 51
+		}
+	},
+	{
+		"KeyName": "LED-TOP-1",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 110,
+		"Position": {
+			"Left": 222,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-2",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 111,
+		"Position": {
+			"Left": 328,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-3",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 112,
+		"Position": {
+			"Left": 433,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-4",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 113,
+		"Position": {
+			"Left": 538,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-5",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 114,
+		"Position": {
+			"Left": 644,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-6",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 115,
+		"Position": {
+			"Left": 750,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-7",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 116,
+		"Position": {
+			"Left": 855,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-TOP-8",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 117,
+		"Position": {
+			"Left": 958,
+			"Top": 130,
+			"Width": 20,
+			"Height": 15
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-1",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 118,
+		"Position": {
+			"Left": 202,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-2",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 119,
+		"Position": {
+			"Left": 312,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-3",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 120,
+		"Position": {
+			"Left": 422,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-4",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 121,
+		"Position": {
+			"Left": 522,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-5",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 122,
+		"Position": {
+			"Left": 632,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-6",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 123,
+		"Position": {
+			"Left": 742,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-7",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 124,
+		"Position": {
+			"Left": 852,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	},
+	{
+		"KeyName": "LED-BOTTOM-8",
+		"Show": "",
+		"LogicCode": -1,
+		"LocationCode": 125,
+		"Position": {
+			"Left": 952,
+			"Top": 470,
+			"Width": 0,
+			"Height": 0
+		}
+	}
+]
